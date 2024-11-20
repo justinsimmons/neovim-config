dd614b7ed9c3004e28583f4bb73a35c38f1de735
Justin Simmons <justin.simmons@airspacelink.com>
2024-11-20 13:41:22 -0500

    Add autopairs for auto-closing developer syntax.

---
+++ lua/plugins/autopairs.lua
@@ -0,0 +1,23 @@
+return {
+    'windwp/nvim-autopairs',
+    event = { "InsertEnter" },
+    dependencies = {
+        "hrsh7th/nvim-cmp",
+    },
+    config = function()
+        local autopairs = require("nvim-autopairs")
+
+        autopairs.setup({
+            check_ts = true, -- Enable treesitter
+            ts_config = {
+                lua = { "string" },
+                javascript = { "template_string" },
+            }
+        })
+
+        -- Make autopairs and completion work together.
+        local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+        local cmp = require('cmp')
+        cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
+    end
+}
