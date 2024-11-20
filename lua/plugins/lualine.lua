7bf51b2a9fbf081f2a122f1a56501697c1ab4d9c
Justin Simmons <justin.simmons@airspacelink.com>
2024-11-20 13:40:23 -0500

    Suppress update notifications and add pending updates to lualine.

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -2,7 +2,10 @@ return {
     "nvim-lualine/lualine.nvim",
     dependencies = { 'nvim-tree/nvim-web-devicons' },
     config = function()
-        require("lualine").setup {
+        local lualine = require("lualine")
+        local lazy_status = require("lazy.status") -- Lazy pending updates count
+
+        lualine.setup {
             options = {
                 icons_enabled = true,
                 theme = 'auto',
@@ -25,7 +28,16 @@ return {
                 lualine_a = { 'mode' },
                 lualine_b = { 'branch', 'diff', 'diagnostics' },
                 lualine_c = { 'filename' },
-                lualine_x = { 'encoding', 'fileformat', 'filetype' },
+                lualine_x = {
+                    {
+                        lazy_status.updates,
+                        cond = lazy_status.has_updates,
+                        color = { fg = "#ff9e64" },
+                    },
+                    { 'encoding' },
+                    { 'fileformat' },
+                    { 'filetype' },
+                },
                 lualine_y = { 'progress' },
                 lualine_z = { 'location' }
             },
