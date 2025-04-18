08774fa82d57c5b409659b7bf81ce51cd1155cb3
Justin Simmons <justin.simmons@airspacelink.com>
2025-04-18 13:49:36 -0400

    Add nvim-surround for HTML development.

---
+++ lua/plugins/nvim-surround.lua
@@ -0,0 +1,8 @@
+return {
+    "kylechui/nvim-surround",
+    -- version = "^3.0.0", -- Use for stability; omit to use `main` branch for the latest features
+    event = { "BufReadPre", "BufNewFile" },
+    opts = {
+        -- Configuration here, or leave empty to use defaults
+    },
+}
