20c3ff0c9176c699a5bf5ce2febf0a3dfdd44ca1
Justin Simmons <justin.simmons@airspacelink.com>
2025-07-23 09:10:43 -0400

    Add ability to change color-schemes mid session.

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -9,7 +9,7 @@ return {
         lualine.setup {
             options = {
                 icons_enabled = true,
-                theme = 'gruvbox-material',
+                theme = 'auto',
                 component_separators = { left = '', right = '' },
                 section_separators = { left = '', right = '' },
                 disabled_filetypes = {
